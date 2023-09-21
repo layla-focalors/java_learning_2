7c2010ab8b5de80bc37c143dd3dd7eef799d0060
arisia <starlights2283@gmail.com>
2023-09-21 16:20:01 +0900

    update

--- n20230921/whiles.java
+++ n20230921/whiles.java
@@ -1,5 +1,7 @@
 package n20230921;
 
+import javax.swing.text.Style;
+
 public class whiles {
     public static void main(String[] args){
         int add = 0;
@@ -10,15 +12,37 @@ public class whiles {
         //         switcher = false;
         //     }
         // }
-        while(add < 21){
-            add += 1;
-            System.out.printf("add = %d\n", add);
+        // while(add < 21){
+        //     add += 1;
+        //     System.out.printf("add = %d\n", add);
+        // }
+        // add = 0;
+        // boolean isrun = false;
+        // do {
+        //     add += 1;
+        //     System.out.println("[do-while] " + add);
+        // }while (!isrun);
+
+        // for(int i = 0; i < 4; i++){
+        //     for(int j = 0; j < 5; j ++){
+        //         if(i == 3 && j == 3){
+        //             System.out.println("before break");
+        //             break;
+        //         }
+        //         System.out.println("after break");
+        //         // continue;
+                
+        //     }
+        // }
+
+        int x = 0;
+        while(x < 100){
+            if(x%2 == 0){
+                x++;
+                continue;
+            }
+            System.out.println(x);
+            x++;
         }
-        add = 0;
-        boolean isrun = false;
-        do {
-            add += 1;
-            System.out.println("[do-while] " + add);
-        }while (!isrun);
     }
 }
