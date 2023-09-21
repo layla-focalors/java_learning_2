486808192cd2bedc93caef871e8fbe1def18f241
arisia <starlights2283@gmail.com>
2023-09-21 15:20:26 +0900

    update pus

--- n20230921/f4.java
+++ n20230921/f4.java
@@ -13,5 +13,17 @@ public class f4 {
         System.out.println(++left == 282 && ++right == 21);
         System.out.println(left);
         System.out.println(right);
+
+        int random_number = 11;
+        String number_type = (random_number%2==0) ? "짝수" : "홀수";
+        System.out.println(number_type);
+
+        int number = 0;
+        number += 5;
+        System.out.println(number);
+
+        int number2 = 0;
+        number2 = +5;
+        System.out.println(number2);
     }
 }
